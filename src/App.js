35dac90089bb1ddb87903c7708007b75a1a732ce
SheharaAbeythunga <shehara.lsa@gmail.com>
2024-01-17 18:16:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+//static variable
+const API_URL='https://www.omdbapi.com?apikey=914eb925';
 
 function App() {
+
+  const searchMovies=async(title)=>{
+      const response=await fetch(`${API_URL}&s=${title}`);
+
+      const data=await response.json();
+      console.log(data.Search);
+  }
+
+  useEffect(()=>{
+    searchMovies('Spiderman');
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>vfdbdfbfd</h1>
     </div>
   );
 }
